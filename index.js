8c2b773e8bd151b768403dc3080de317d9ec8136
LouBNielsen <louise.b.nielsen@hotmail.com>
2018-03-28 13:22:56 +0200

    Login system

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var helmet = require('helmet')
+var bcrypt = require('bcrypt');
+
+app.use(helmet())
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -29,14 +33,12 @@ app.post('/CheckUser', function (req, res) {
     switch (LogInType) {
         case 'Unsafe':
             con.connect(function (err) {
-                if (err) throw err;
+                if (err) throw err
 
                 var sql = "SELECT count(*) as Count FROM user WHERE username = '" + UserName + "' AND password = '" + Password + "';"
 
-                console.log(sql)
-
-                con.query(sql, function (err, result, fields) {
-                    if (err) throw err;
+                con.query(sql, function (err, result) {
+                    if (err) throw err
 
                     if (result[0].Count > 0) {
                         UserLoggedIn = true;
@@ -51,14 +53,14 @@ app.post('/CheckUser', function (req, res) {
 
         case 'Safer':
             con.connect(function (err) {
-                if (err) throw err;
+                if (err) throw err
 
                 var sql = "SELECT count(*) as Count FROM user WHERE username = ? AND password = ?;"
 
                 console.log(sql)
 
-                con.query(sql, [UserName, Password], function (err, result, fields) {
-                    if (err) throw err;
+                con.query(sql, [UserName, Password], function (err, result) {
+                    if (err) throw err
 
                     if (result[0].Count > 0) {
                         UserLoggedIn = true;
@@ -71,6 +73,57 @@ app.post('/CheckUser', function (req, res) {
             });
             break;
 
+        case 'SaveUser':
+            con.connect(function (err) {
+                if (err) throw err
+
+                var salt = bcrypt.genSaltSync(10);
+
+                bcrypt.genSalt(10, function (err, salt) {
+                    if (err) throw err
+                    
+                    bcrypt.hash(Password, salt, function (err, hash) {
+                        if (err) throw err
+
+                        var sql = "INSERT INTO user (username, password) VALUES ?;"
+                        var user = [
+                            [UserName, hash+salt]
+                        ];
+                        con.query(sql, [user], function (err, result) {
+                            if (err) throw err
+
+                            res.send('User name: ' + UserName + ' - Password: ' + hash + ' - has been created...')
+
+                        });
+                    });
+                });
+            })
+            break;
+
+        case 'EncryptedLogIn':
+            con.connect(function (err) {
+                if (err) throw err
+
+                var sql = "SELECT password FROM user WHERE username = ?"
+
+                con.query(sql, [UserName], function (err, result) {
+                    if (err) throw err
+
+                    var encryptedPassword = result[0].password
+
+                    bcrypt.compare(Password, encryptedPassword, function (err, result) {
+                        if (err) res.send(err)
+
+                        if (result) {
+                            UserLoggedIn = true;
+
+                            res.send('User name: ' + UserName + ' - Log in type: ' + LogInType + '<br><br>User is logged in...')
+                        } else {
+                            res.send('User name: ' + UserName + ' - Log in type: ' + LogInType + '<br><br>User is NOT logged in...')
+                        }
+                    });
+                });
+            });
         default:
             'Error'
     }
